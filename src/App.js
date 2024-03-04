4a2f40f67c43958c43dfb371d5c207012a4e730d
Atef Lachhab <atef.lachhab1@gmail.com>
2024-03-04 15:50:16 -0500

    footer div

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
-import s from "./style.module.css"
+import s from "./style.module.css";
 
 export function App() {
   return (
-    <div className={s.container}>
-      <Header />
-      <Outlet />
+    <div>
+      <div className={s.container}>
+        <Header />
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
