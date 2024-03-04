ee0dfd18d3d2625ee63c053a2aa876b987384103
Atef Lachhab <atef.lachhab1@gmail.com>
2024-03-04 16:00:13 -0500

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,11 @@
 import s from "./style.module.css";
+import logo from "../../assets/images/logo-footer.png";
 
 export function Footer() {
-  return <div className={s.footer}>Footer</div>;
+  return (
+    <div className={s.footer}>
+      <img src={logo} alt="Logo de Kasa" />
+      <p>© 2020 Kasa. All rights reserved</p>
+    </div>
+  );
 }
