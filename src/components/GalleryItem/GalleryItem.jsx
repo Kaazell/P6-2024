8b259a1231f4a552891c2b70914032d1b559b703
Atef Lachhab <atef.lachhab1@gmail.com>
2024-03-04 15:35:17 -0500

    GalleryItem

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,5 +1,9 @@
 import s from "./style.module.css";
 
 export function GalleryItem() {
-  return <div className={s.galleryItem}>GalleryItem</div>;
+  return (
+    <div className={s.galleryItem}>
+      <span>Titre de la location</span>
+    </div>
+  );
 }
