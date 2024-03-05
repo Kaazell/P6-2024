c0dcf95790994c2aeaf028c246694a874e98fd6c
Atef Lachhab <atef.lachhab1@gmail.com>
2024-03-05 14:36:18 -0500

    chevron animation

--- src/components/DropdownButton/DropdownButton.jsx
+++ src/components/DropdownButton/DropdownButton.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import { ChevronUp } from "react-bootstrap-icons";
 import s from "./style.module.css";
+
 export function DropdownButton({ buttonText }) {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -15,6 +17,12 @@ export function DropdownButton({ buttonText }) {
     <div className={s.container}>
       <button className={s.button_title} onClick={toggleDropdown}>
         {buttonText}
+        <ChevronUp
+          style={{
+            transform: isOpen ? "rotate(180deg)" : "rotate(0deg)",
+            transition: "transform 0.3s ease",
+          }}
+        />
       </button>
       {isOpen && (
         <div className={s.list}>
