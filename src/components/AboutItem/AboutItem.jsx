a04b25ea531564b2a2794898ea4bfdfcdb347b75
Atef Lachhab <atef.lachhab1@gmail.com>
2024-03-11 22:44:28 -0400

    About page

--- src/components/AboutItem/AboutItem.jsx
+++ src/components/AboutItem/AboutItem.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { ChevronUp } from "react-bootstrap-icons";
 import s from "./style.module.css";
-export function AboutItem() {
+export function AboutItem({ title, content }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleDropdown = () => setIsOpen(!isOpen);
@@ -9,7 +9,7 @@ export function AboutItem() {
   return (
     <div className={s.container}>
       <button className={s.button_title} onClick={toggleDropdown}>
-        AboutItem
+        {title}
         <ChevronUp
           style={{
             transform: isOpen ? "rotate(180deg)" : "rotate(0deg)",
@@ -17,7 +17,9 @@ export function AboutItem() {
           }}
         />
       </button>
-      <div className={`${s.list} ${isOpen ? s.open : ""}`}>hello</div>
+      <div className={`${s.list} ${isOpen ? s.open : ""}`}>
+        <p>{content}</p>
+      </div>
     </div>
   );
 }
